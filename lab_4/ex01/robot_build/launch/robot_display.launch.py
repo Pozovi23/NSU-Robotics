9e661500750cae10411aca52441b909092349758
Pozovi23 <gleb.zhigalov.2005@mail.ru>
2025-11-04 15:56:59 +0700

    urdf to xacro.urdf

--- lab_4/ex01/robot_build/launch/robot_display.launch.py
+++ lab_4/ex01/robot_build/launch/robot_display.launch.py
@@ -1,26 +1,20 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    # Получаем путь к пакету robot_build
     pkg_share = get_package_share_directory('robot_build')
 
-    # Определяем пути к файлам относительно пакета
-    default_model_path = os.path.join(pkg_share, 'src', 'description', 'robot.urdf')
+    default_model_path = os.path.join(pkg_share, 'src', 'description', 'robot.xacro.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'robot.rviz')
 
     print(f"Looking for URDF at: {default_model_path}")
     print(f"Looking for RViz config at: {default_rviz_config_path}")
-
-
-    with open(default_model_path, 'r') as f:
-        robot_description = f.read()
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -28,7 +22,7 @@ def generate_launch_description():
         name='robot_state_publisher',
         output='screen',
         parameters=[{
-            'robot_description': robot_description
+            'robot_description': Command(['xacro ', LaunchConfiguration('model')])
         }]
     )
 
@@ -59,6 +53,11 @@ def generate_launch_description():
             name='gui',
             default_value='True',
             description='Flag to enable joint_state_publisher_gui'
+        ),
+        DeclareLaunchArgument(
+            name='model',
+            default_value=default_model_path,
+            description='Absolute path to robot urdf file'
         ),
         DeclareLaunchArgument(
             name='rvizconfig',
